2bd1b4d5becf2df7ff8d4d130096d2a3ee983885
Zi Yang Fu <ziyang.fu@ncs.com.sg>
2024-10-30 01:12:33 +0800

    import and add RDS CA certificate as trust store of apps

--- src/libs/database.ts
+++ src/libs/database.ts
@@ -1,6 +1,15 @@
 import { createPool } from 'mysql2/promise';
+import * as fs from 'fs';
+import * as path from 'path';
 
-const sslConfig = JSON.parse(process.env.DB_SSL || '{"rejectUnauthorized": true}');
+// Parse DB_SSL environment variable
+const sslConfig = process.env.DB_SSL === 'true' 
+  ? {
+      rejectUnauthorized: true,
+      ca: fs.readFileSync(path.join(__dirname, './ap-southeast-1-bundle.pem'))
+    }
+  : {"rejectUnauthorized": false};
+
 // Database connection configuration
 const dbConfig = {
   host: process.env.DB_HOST,
