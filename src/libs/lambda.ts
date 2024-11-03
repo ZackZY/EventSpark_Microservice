6d193e75f93d100936cdd958a11a5bd17775a657
Zi Yang Fu <ziyang.fu@ncs.com.sg>
2024-11-03 21:36:28 +0800

    Test CORS

--- src/libs/lambda.ts
+++ src/libs/lambda.ts
@@ -1,6 +1,17 @@
 import middy from "@middy/core"
+import cors from "@middy/http-cors";
 import middyJsonBodyParser from "@middy/http-json-body-parser"
 
 export const middyfy = (handler) => {
-  return middy(handler).use(middyJsonBodyParser())
-}
+  return middy(handler)
+    .use(middyJsonBodyParser())
+    .use(cors({
+      credentials: true,
+      headers: 'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token',
+      origins: [
+        process.env.IS_OFFLINE === 'true' 
+          ? 'http://localhost:3000' 
+          : process.env.FRONTEND_URL
+      ]
+    }));
+};
